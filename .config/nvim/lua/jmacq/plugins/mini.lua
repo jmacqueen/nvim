035d56b5ec3e7375210adf46220628d8dfaa9ee9
Jonathan MacQueen <jonathan@jmacqueen.com>
2025-01-23 18:19:59 -0500

    Move more functions over to mini.nvim

--- .config/nvim/lua/jmacq/plugins/mini.lua
+++ .config/nvim/lua/jmacq/plugins/mini.lua
@@ -7,6 +7,15 @@ return {
       require("mini.hues").setup({ background = "#19213a", foreground = "#c4c6cd" })
       require("mini.operators").setup()
       require("mini.surround").setup()
+      require("mini.cursorword").setup()
+      require("mini.indentscope").setup()
+
+      local hipatterns = require("mini.hipatterns")
+      hipatterns.setup({
+        highlighters = {
+          hex_color = hipatterns.gen_highlighter.hex_color(),
+        },
+      })
 
       vim.cmd([[colorscheme randomhue]])
     end,
