e2eb431b2eeade607750f27453ae7919b3771d09
Jonathan MacQueen <jonathan.macqueen@bitsighttech.com>
2025-05-01 11:46:42 -0400

    fix formatting in Snacks config

--- .config/nvim/lua/jmacq/plugins/snacks.lua
+++ .config/nvim/lua/jmacq/plugins/snacks.lua
@@ -10,195 +10,36 @@ return {
   },
   keys = {
     -- Top pickers
-    {
-      "<leader><leader>",
-      function()
-        Snacks.picker.buffers({ sort_lastused = false })
-      end,
-      desc = "Buffers",
-    },
-    {
-      "<leader>:",
-      function()
-        Snacks.picker.command_history()
-      end,
-      desc = "Search command history",
-    },
-    {
-      "<leader>\\",
-      function()
-        Snacks.picker.explorer()
-      end,
-      desc = "File explorer",
-    },
+    { "<leader><leader>", function() Snacks.picker.buffers({ sort_lastused = false }) end, desc = "Buffers", },
+    { "<leader>:", function() Snacks.picker.command_history() end, desc = "Search command history", },
+    { "<leader>\\", function() Snacks.picker.explorer() end, desc = "File explorer", },
     -- Explorer
-    {
-      "<leader>ee",
-      function()
-        Snacks.picker.explorer()
-      end,
-      desc = "File [e]xplorer",
-    },
+    { "<leader>ee", function() Snacks.picker.explorer() end, desc = "File [e]xplorer", },
     -- Find
-    {
-      "<leader>fb",
-      function()
-        Snacks.picker.buffers({ sort_lastused = false })
-      end,
-      desc = "[B]uffers",
-    },
-    {
-      "<leader>fc",
-      function()
-        Snacks.picker.colorschemes()
-      end,
-      desc = "[C]olorschemes",
-    },
-    {
-      "<leader>fe",
-      function()
-        Snacks.picker.explorer()
-      end,
-      desc = "File [e]xplorer",
-    },
-    {
-      "<leader>ff",
-      function()
-        Snacks.picker.files()
-      end,
-      desc = "Find [f]iles",
-    },
-    {
-      "<leader>fg",
-      function()
-        Snacks.picker.grep()
-      end,
-      desc = "Live [g]rep for string in cwd",
-    },
-    {
-      "<leader>fh",
-      function()
-        Snacks.picker.help()
-      end,
-      desc = "[H]elp pages",
-    },
-    {
-      "<leader>fk",
-      function()
-        Snacks.picker.keymaps()
-      end,
-      desc = "[K]eymaps",
-    },
-    {
-      "<leader>fs",
-      function()
-        Snacks.picker.lsp_symbols()
-      end,
-      desc = "LSP [s]ymbols",
-    },
-    {
-      "<leader>fp",
-      function()
-        Snacks.picker.projects()
-      end,
-      desc = "[P]rojects",
-    },
-    {
-      "<leader>fr",
-      function()
-        Snacks.picker.recent()
-      end,
-      desc = "Find [r]ecent files",
-    },
-    {
-      "<leader>fw",
-      function()
-        Snacks.picker.grep_word()
-      end,
-      desc = "Find [w]ord under cursor in cwd",
-      mode = { "n", "x" },
-    },
+    { "<leader>fb", function() Snacks.picker.buffers({ sort_lastused = false }) end, desc = "[B]uffers", },
+    { "<leader>fc", function() Snacks.picker.colorschemes() end, desc = "[C]olorschemes", },
+    { "<leader>fe", function() Snacks.picker.explorer() end, desc = "File [e]xplorer", },
+    { "<leader>ff", function() Snacks.picker.files() end, desc = "Find [f]iles", },
+    { "<leader>fg", function() Snacks.picker.grep() end, desc = "Live [g]rep for string in cwd", },
+    { "<leader>fh", function() Snacks.picker.help() end, desc = "[H]elp pages", },
+    { "<leader>fk", function() Snacks.picker.keymaps() end, desc = "[K]eymaps", },
+    { "<leader>fs", function() Snacks.picker.lsp_symbols() end, desc = "LSP [s]ymbols", },
+    { "<leader>fp", function() Snacks.picker.projects() end, desc = "[P]rojects", },
+    { "<leader>fr", function() Snacks.picker.recent() end, desc = "Find [r]ecent files", },
+    { "<leader>fw", function() Snacks.picker.grep_word() end, desc = "Find [w]ord under cursor in cwd", mode = { "n", "x" }, },
     -- LSP
-    {
-      "gd",
-      function()
-        Snacks.picker.lsp_definitions()
-      end,
-      desc = "Goto [d]efinition",
-    },
-    {
-      "gD",
-      function()
-        Snacks.picker.lsp_declarations()
-      end,
-      desc = "Goto [D]eclaration",
-    },
-    {
-      "gr",
-      function()
-        Snacks.picker.lsp_references()
-      end,
-      nowait = true,
-      desc = "[R]eferences",
-    },
-    {
-      "gI",
-      function()
-        Snacks.picker.lsp_implementations()
-      end,
-      desc = "Goto [I]mplementation",
-    },
-    {
-      "gy",
-      function()
-        Snacks.picker.lsp_type_definitions()
-      end,
-      desc = "Goto T[y]pe Definition",
-    },
+    { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto [d]efinition", },
+    { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto [D]eclaration", },
+    { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "[R]eferences", },
+    { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto [I]mplementation", },
+    { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition", },
 
-    {
-      "<leader>bd",
-      function()
-        Snacks.bufdelete()
-      end,
-      desc = "Delete Buffer",
-    },
-    {
-      "<leader>gb",
-      function()
-        Snacks.git.blame_line()
-      end,
-      desc = "Git Blame Line",
-    },
-    {
-      "<leader>gB",
-      function()
-        Snacks.gitbrowse()
-      end,
-      desc = "Git Browse",
-      mode = { "n", "v" },
-    },
-    {
-      "<leader>gf",
-      function()
-        Snacks.lazygit.log_file()
-      end,
-      desc = "Lazygit Current File History",
-    },
-    {
-      "<leader>gg",
-      function()
-        Snacks.lazygit()
-      end,
-      desc = "Lazygit",
-    },
-    {
-      "<leader>gl",
-      function()
-        Snacks.lazygit.log()
-      end,
-      desc = "Lazygit Log (cwd)",
-    },
+    { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer", },
+    { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line", },
+    { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" }, },
+    { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History", },
+    { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit", },
+    { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)", },
   },
   init = function()
     local Snacks = require("snacks")
