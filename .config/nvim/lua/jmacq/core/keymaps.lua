11f86583adbdbbd4af8b8132910bac421faebf7e
Jonathan MacQueen <jonathan@jmacqueen.com>
2025-05-29 17:53:05 -0400

    Add C-L jump to end of Treesitter node in insert

--- .config/nvim/lua/jmacq/core/keymaps.lua
+++ .config/nvim/lua/jmacq/core/keymaps.lua
@@ -46,3 +46,12 @@ keymap.set("n", "gf", ":vert winc f<cr>")
 keymap.set("n", "yp", ":let @+=expand('%:p')<CR>", { silent = true })
 -- " copies pwd to clipboard: command yd
 keymap.set("n", "yd", ":let @+=expand('%:p:h')<CR>", { silent = true })
+
+-- use <C-L> in insert mode to jump to end of current treesitter node
+keymap.set("i", "<C-L>", function()
+  local node = vim.treesitter.get_node()
+  if node ~= nil then
+    local row, col = node:end_()
+    pcall(vim.api.nvim_win_set_cursor, 0, { row + 1, col })
+  end
+end, { desc = "insjump" })
